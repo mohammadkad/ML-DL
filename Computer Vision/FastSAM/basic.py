169d0051700cc7eb0f70114b8ab35e3c37e88096
Mohammad Kadkhodaei <mohammadkad@gmail.com>
2026-09-22 09:31:07 +0330

    Create basic.py

---
+++ Computer Vision/FastSAM/basic.py
@@ -0,0 +1,20 @@
+# Define an inference source
+source = "dogs.jpg"
+
+# Create a FastSAM model
+model = FastSAM("FastSAM-s.pt")  # or FastSAM-x.pt
+
+# Run inference on an image
+everything_results = model(source, device="cpu", retina_masks=True, imgsz=1024, conf=0.4, iou=0.9)
+
+# Run inference with bboxes prompt
+results = model(source, bboxes=[439, 437, 524, 709])
+
+# Run inference with points prompt
+results = model(source, points=[[200, 200]], labels=[1])
+
+# Run inference with texts prompt
+results = model(source, texts="a photo of a dog")
+
+# Run inference with bboxes and points and texts prompt at the same time
+results = model(source, bboxes=[439, 437, 524, 709], points=[[200, 200]], labels=[1], texts="a photo of a dog")
